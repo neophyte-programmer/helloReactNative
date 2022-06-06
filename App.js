d07c097305a6ecbae728062c9ea4c9ffa4fcacc2
Nutifafa Attor <attorfafa@gmail.com>
2022-06-06 21:37:46 +0000

    learning layouts in React native

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Another piece of text</Text>
-			<Text>My name is Nutifafa</Text>
-			<Button title='Click Here' />
+  return (
+    // Stores the different content of the app
+    <View style={styles.appContainer}>
+      {/* Add The Goals Here */}
+      <View style={styles.addGoalContainer}>
+        <TextInput placeholder='Enter your goal!' style={styles.textInput} />
+        <Button title='Add Goal' />
+      </View>
+
+      {/* Store The List of Goals here */}
+      <View>
+        <Text>Goals</Text>
+      </View>
+      
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	text: {
-		marginBottom: 16,
-		borderWidth: 2,
-		borderColor: '#fab2cd',
-    padding: 10,
-    color: '#fab2cd',
-	},
+  appContainer: {
+    margin: 50,
+  },
+  addGoalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    width: '80%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 5,
+    padding: 5,
+  },
 })
