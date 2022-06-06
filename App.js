73dd357add2dca5b660750845af08863e407d4e3
Nutifafa Attor <attorfafa@gmail.com>
2022-06-06 19:05:35 +0000

    learning ui components and stylesheet

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Another piece of text</Text>
+			<Text>My name is Nutifafa</Text>
+			<Button title='Click Here' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		marginBottom: 16,
+		borderWidth: 2,
+		borderColor: '#fab2cd',
+    padding: 10,
+    color: '#fab2cd',
+	},
+})
